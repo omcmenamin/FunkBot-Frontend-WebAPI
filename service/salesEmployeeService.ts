11290c2eea909b2a87ebc753224b55000b07c8ad
Morgan Mcdonald <morgano1312@gmail.com>
2023-08-31 12:15:56 +0100

    Update salesEmployeeService.ts

--- service/salesEmployeeService.ts
+++ service/salesEmployeeService.ts
@@ -4,20 +4,20 @@ const axios = require('axios');
 module.exports.getSalesEmployee= async function () {
 
     try {
-        const response = await axios.get('http://localhost:8080/api/orders')
+        const response = await axios.get('http://localhost:8080/api/salesEmployee')
 
         return response.data
     } catch (e){
-        return new Error('Could not get orders')
+        return new Error('Could not get Sales Employee')
     }
 }
 
 module.exports.getSalesEmployeeById = async function (id: number): Promise<SalesEmployee> {
     try{
-        const response = await axios.get('http://localhost:8080/api/order/' + id)
+        const response = await axios.get('http://localhost:8080/api/salesEmployee/' + id)
     
         return response.data
     }catch(e){
-        throw new Error ('Could not get order')
+        throw new Error ('Could not get Sales Employee')
     }
 }
