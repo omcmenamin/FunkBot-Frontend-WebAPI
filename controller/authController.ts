25b571f28990ba93ba6145283aae0d8d4928d5c9
Orlaith McMenamin <orlaith.mcmenamin@kainos.com>
2023-08-31 15:53:01 +0100

    updating title

--- controller/authController.ts
+++ controller/authController.ts
@@ -15,6 +15,8 @@ module.exports = function (app: Application){
         try{
             req.session.token = await authService.login(data)
 
+            res.redirect('/deliveryEmployees')
+
         }catch (e) {
             console.error(e);
 
